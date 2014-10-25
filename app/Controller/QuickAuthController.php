db9d8a531f5b2e2d88da6e929a35989a4b23c8a0
WhiteThunder <WheteThunger@gmail.com>
2014-10-25 13:56:17 -0700

    Added documentation

--- app/Controller/QuickAuthController.php
+++ app/Controller/QuickAuthController.php
@@ -2,184 +2,229 @@
 App::uses('AppController', 'Controller');
 
 /**
- * Users Controller
+ * QuickAuth Controller
  *
  * @property QuickAuth $QuickAuth
  * @property PaginatorComponent $Paginator
  */
 class QuickAuthController extends AppController {
-	public $components = array('Paginator', 'RequestHandler');
-	public $helpers = array('Html', 'Form', 'Js', 'Time');
+    public $components = array('Paginator', 'RequestHandler');
+    public $helpers = array('Html', 'Form', 'Js', 'Time');
 
-	public function view() {
+    /**
+     * Shows the QuickAuth records page for admins.
+     */
+    public function view() {
 
-		if (!$this->Access->check('QuickAuth', 'read')) {
-			$this->redirect($this->referer());
-			return;
-		}
+        if (!$this->Access->check('QuickAuth', 'read')) {
+            $this->redirect($this->referer());
+            return;
+        }
 
-		$this->loadModel('Server');
-		$servers = Hash::combine($this->Server->find('all', array(
-			'fields' => array(
-				'server_ip', 'name', 'short_name'
-			),
-			'conditions' => array(
-				'server_ip is not null'
-			)
-		)), '{n}.Server.server_ip', '{n}.Server');
+        $this->loadModel('Server');
+        $servers = Hash::combine($this->Server->find('all', array(
+            'fields' => array(
+                'server_ip', 'name', 'short_name'
+            ),
+            'conditions' => array(
+                'server_ip is not null'
+            )
+        )), '{n}.Server.server_ip', '{n}.Server');
 
-		$this->set(array(
-			'tokenExpire' => Configure::read('Store.QuickAuth.TokenExpire'),
-			'servers' => $servers
-		));
+        $this->set(array(
+            'tokenExpire' => Configure::read('Store.QuickAuth.TokenExpire'),
+            'servers' => $servers
+        ));
 
-		$this->records();
-	}
+        $this->records();
+    }
 
-	public function records() {
+    /**
+     * Shows a page of QuickAuth records. Called by view action or via ajax directly.
+     */
+    public function records() {
 
-		if (!$this->Access->check('QuickAuth', 'read')) {
-			if ($this->request->is('ajax')) {
-				$this->autoRender = false;
-			} else {
-				$this->redirect($this->referer());
-			}
-			return;
-		}
+        if (!$this->Access->check('QuickAuth', 'read')) {
+            if ($this->request->is('ajax')) {
+                $this->autoRender = false;
+            } else {
+                $this->redirect($this->referer());
+            }
+            return;
+        }
 
-		$this->Paginator->settings = array(
-			'QuickAuth' => array(
-				'limit' => 25,
-			)
-		);
+        $this->Paginator->settings = array(
+            'QuickAuth' => array(
+                'limit' => 25,
+            )
+        );
 
-		$quickauth = Hash::extract($this->Paginator->paginate('QuickAuth'), '{n}.QuickAuth');
-		$this->addPlayers(Hash::extract($quickauth, '{n}.user_id'));
+        $quickauth = Hash::extract($this->Paginator->paginate('QuickAuth'), '{n}.QuickAuth');
+        $this->addPlayers($quickauth, '{n}.user_id');
 
-		$this->set(array(
-			'quickauth' => $quickauth,
-			'pageModel' => $this->QuickAuth->name,
-			'pageLocation' => array('controller' => 'QuickAuth', 'action' => 'records')
-		));
-	}
+        $this->set(array(
+            'quickauth' => $quickauth,
+            'pageModel' => $this->QuickAuth->name,
+            'pageLocation' => array('controller' => 'QuickAuth', 'action' => 'records')
+        ));
+    }
 
-	/**
-	 * Handles quick authentication when the person's steamid is known such as in a game server.
-	 *
-	 * Expects query parameters 'id' and 'token'.
-	 *
-	 * A weird bug in the Source engine causes the page to be requested twice so re-using tokens is silently ignored.
-	 */
-	public function auth() {
+    /**
+     * Handles quick authentication when a person's steamid is known such as in a game server.
+     *
+     * How to use on a remote server:
+     *
+     *   1) Connect remotely to the store database and save a record to the quick_auth table with the following fields:
+     *
+     *        token: randomly generated positive integer
+     *        user_id: the player's signed 32-bit SteamID - for SourceMod use GetSteamAccountID()
+     *        server: the server ip they are coming from (or a string like 'forums' if coming from a website)
+     *        is_member: optional boolean for whether you know the user is an rxg member (e.g., csgo rxg tag)
+     *
+     *   2) Use LAST_INSERT_ID() to get the id of the new record
+     *
+     *   3) Send the user to /quickauth with the following query parameters:
+     *
+     *        id: the record id obtained from step #2
+     *        token: the token you saved in step #1
+     *        source: optional abbreviated name of the game mod (e.g., 'csgo', 'tf2')
+     *
+     * The random token does not have to be unique because when paired with the unique id, the combination will be both
+     * unique and random.
+     *
+     * Once the user is sent to the URL, the rest of the login process will be handled by this action and the user will
+     * be sent to the main store page. If the user is coming from a game server that requires a popup to properly show
+     * the window (e.g., a CS:GO server), the 'source' query parameter specified in the URL must be equal to one of the
+     * values in the Store.QuickAuth.PopupFromServers array in the bootstrap.php config or it will not work.
+     *
+     * Note: You do not need to worry about timezones and clock synchronization because the date field is set by the
+     * database when you insert the token as long as your script does not specify it. To configure the amount of time
+     * tokens are usable, edit Store.QuickAuth.TokenExpire in the bootstrap.php config.
+     *
+     * Note: A bug in the Source engine causes the page to be requested twice so re-using tokens is silently ignored.
+     */
+    public function auth() {
 
-		$params = $this->request->query;
-		$redirLoc = array('controller' => 'Items', 'action' => 'index');
+        $params = $this->request->query;
 
-		if (empty($params)) {
-			CakeLog::write('quickauth', "QuickAuth attempted with no URL params.");
-			$this->redirect($redirLoc);
-			return;
-		}
+        // default redirect location (main store page)
+        $redirLoc = array('controller' => 'Items', 'action' => 'index');
 
-		$user = $this->Auth->user('user_id');
-		$config = Configure::read('Store.QuickAuth');
+        if (empty($params)) {
+            CakeLog::write('quickauth', "QuickAuth attempted with no query string.");
+            $this->redirect($redirLoc);
+            return;
+        }
 
-		$tokenId = $params['id'];
-		$tokenValue = $params['token'];
+        if (empty($params['id']) || empty($params['token'])) {
+            CakeLog::write('quickauth', "QuickAuth attempted with incomplete query string.");
+            $this->redirect($redirLoc);
+            return;
+        }
 
-		$auth = Hash::extract($this->QuickAuth->find('first', array(
-			'conditions' => array(
-				'quick_auth_id' => $tokenId,
-				'token' => $tokenValue
-			)
-		)), 'QuickAuth');
+        $user = $this->Auth->user('user_id');
+        $config = Configure::read('Store.QuickAuth');
 
-		if (!empty($auth)) {
+        $tokenId = $params['id'];
+        $tokenValue = $params['token'];
 
-			//Silently ignore if already redeemed (probably double request)
-			if (!$auth['redeemed']) {
+        $auth = Hash::extract($this->QuickAuth->find('first', array(
+            'conditions' => array(
+                'quick_auth_id' => $tokenId,
+                'token' => $tokenValue
+            )
+        )), 'QuickAuth');
 
-				$this->QuickAuth->id = $auth['quick_auth_id'];
-				$this->QuickAuth->saveField('redeemed', 1);
+        if (!empty($auth)) {
 
-				$user_id = $auth['user_id'];
+            // silently ignore if already redeemed (probably double request)
+            if (!$auth['redeemed']) {
 
-				$aro = $this->Access->findUser($user_id);
+                // set token as redeemed in db
+                $this->QuickAuth->id = $auth['quick_auth_id'];
+                $this->QuickAuth->saveField('redeemed', 1);
 
-				if (empty($aro)) {
-					//Record user as member
-					CakeLog::write('quickauth', "Promoted user $user_id to member.");
-					$aro->save(array(
-						'parent_id' => 1,
-						'model' => 'User',
-						'foreign_key' => $user_id
-					));
-				}
+                $user_id = $auth['user_id'];
 
-				if (empty($user)) {
+                // if confirmed member, promote to member if not already set
+                if ($auth['is_member']) {
 
-					//not already logged in
-					$diff = strtotime($auth['date']) + $config['TokenExpire'] - time();
+                    $aro = $this->Access->findUser($user_id);
 
-					if ($diff < 0) {
+                    if (empty($aro)) {
+                        CakeLog::write('quickauth', "Promoted user $user_id to member.");
+                        $aro->save(array(
+                            'parent_id' => 1,
+                            'model' => 'User',
+                            'foreign_key' => $user_id
+                        ));
+                    }
+                }
 
-						//Expired unredeemed token
-						$diff = abs($diff);
-						CakeLog::write('quickauth', "Attempted usage of token $tokenId-$tokenValue which expired $diff seconds ago.");
-						$this->Session->setFlash('Authentication token expired. Please contact an administrator.', 'default', array('class' => 'error'));
+                // do login process if not already logged in
+                if (empty($user)) {
 
-					} else if (!$this->AccountUtility->loginUser($user_id, array('force' => true))) {
+                    $diff = strtotime($auth['date']) + $config['TokenExpire'] - time();
 
-						//Failed to login user
-						CakeLog::write('quickauth', "Failed to login user $user_id with token $tokenId-$tokenValue.");
-						$this->Session->setFlash('Login failed. Please contact an administrator.', 'default', array('class' => 'error'));
-					}
+                    if ($diff < 0) {
 
-				} else {
+                        // token expired
+                        $diff = abs($diff);
+                        CakeLog::write('quickauth', "Attempted usage of token $tokenId-$tokenValue which expired $diff seconds ago.");
+                        $this->Session->setFlash('Authentication token expired. Please contact an administrator.', 'default', array('class' => 'error'));
 
-					//User already logged in
-					CakeLog::write('quickauth', "Authentication for token $tokenId-$tokenValue skipped. User already logged in.");
-				}
-			}
+                    } else if (!$this->AccountUtility->loginUser($user_id, AccountUtilityComponent::LOGIN_FORCE)) {
 
-		} else if (empty($user)) {
+                        // failed to login user
+                        CakeLog::write('quickauth', "Failed to login user $user_id with token $tokenId-$tokenValue.");
+                        $this->Session->setFlash('Login failed. Please contact an administrator.', 'default', array('class' => 'error'));
+                    }
 
-			//Token not found in db, not already logged in
-			CakeLog::write('quickauth', "Requested token $tokenId-$tokenValue was not found.");
-			$this->Session->setFlash('Invalid Authentication token. Please contact an administrator.', 'default', array('class' => 'error'));
-		}
+                } else {
 
-		$this->Session->write('Auth.user.ingame', true);
+                    // user already logged in
+                    CakeLog::write('quickauth', "Authentication for token $tokenId-$tokenValue skipped. User already logged in.");
+                }
+            }
 
-		if (empty($params['source'])) {
-			//Go straight to store
-			$this->redirect($redirLoc);
-			return;
-		}
+        } else if (empty($user)) {
 
-		//Find name of server for url
-		if (!empty($auth)) {
-			$this->loadModel('Server');
-			$server = Hash::extract($this->Server->findByServerIp($auth['server'], array('short_name')), 'Server');
-		}
+            // token not found in db, nor is used logged in already
+            CakeLog::write('quickauth', "Requested token $tokenId-$tokenValue was not found.");
+            $this->Session->setFlash('Invalid Authentication token. Please contact an administrator.', 'default', array('class' => 'error'));
+        }
 
-		//if server not found, use source from url
-		$server = !empty($server) ? $server['short_name'] : $params['source'];
-		$redirLoc = array('controller' => 'Items', 'action' => 'index', 'server' => $server);
+        $this->Session->write('Auth.user.ingame', true);
 
-		if (!in_array($params['source'], $config['PopupFromServers'])) {
-			//Go straight to store
-			$this->redirect($redirLoc);
-			return;
-		}
+        // go straight to store if source not specified
+        if (empty($params['source'])) {
+            $this->redirect($redirLoc);
+            return;
+        }
 
-		//Render page with JS popup
-		$this->set(array(
-			'popupUrl' => $redirLoc,
-			'height' => $config['WindowHeight'],
-			'width' => $config['WindowWidth']
-		));
+        // find name of server for url
+        if (!empty($auth)) {
+            $this->loadModel('Server');
+            $server = Hash::extract($this->Server->findByServerIp($auth['server'], array('short_name')), 'Server');
+        }
 
-		$this->render('/Common/popup');
-	}
+        // if server not found, use source from url
+        $server = !empty($server) ? $server['short_name'] : $params['source'];
+        $redirLoc = array('controller' => 'Items', 'action' => 'index', 'server' => $server);
+
+        // if server is not in the popup list, send user straight to store
+        if (!in_array($params['source'], $config['PopupFromServers'])) {
+            $this->redirect($redirLoc);
+            return;
+        }
+
+        // render page with JS popup
+        $this->set(array(
+            'popupUrl' => $redirLoc,
+            'height' => $config['WindowHeight'],
+            'width' => $config['WindowWidth']
+        ));
+
+        $this->render('/Common/popup');
+    }
 }
