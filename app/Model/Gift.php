07dd041ae12e6ff9202e028eb91e31904e63e424
WhiteThunder <WheteThunger@gmail.com>
2014-12-06 10:02:32 -0800

    Moved accept gift logic to model, added missing property in comments

--- app/Model/Gift.php
+++ app/Model/Gift.php
@@ -4,6 +4,7 @@ App::uses('AppModel', 'Model');
  * Gift Model
  *
  * @property Activity $Activity
+ * @property GiftDetail $GiftDetail
  * @property User $Sender
  * @property User $Recipient
  */
@@ -29,6 +30,94 @@ class Gift extends AppModel {
     );
 
     public $order = 'Gift.gift_id DESC';
+
+
+    /**
+     * Accepts a pending gift specified by $gift_id for the provided $recipient_id if one exists. This will add the
+     * items from the gift details to the user's inventory. If the $gift_id does not match the $user_id or if it was
+     * already accepted (or not found), this will return false instead of the gift data.
+     *
+     * @param int $gift_id the id of the gift to accept
+     * @param int $recipient_id the user_id of the recipient for double-checking if that person was accepting it
+     * @return array|false the gift that was accepted, or false if no pending gift was found with the specified ids
+     */
+    public function acceptPendingGift($gift_id, $recipient_id) {
+
+        $UserItem = $this->Recipient->UserItem;
+
+        $gift = $this->find('first', array(
+            'conditions' => array(
+                'gift_id' => $gift_id,
+                'recipient_id' => $recipient_id,
+                'accepted = 0'
+            ),
+            'contain' => 'GiftDetail'
+        ));
+
+        // check if matching gift was found
+        if (!empty($gift)) {
+
+            // lock user inventory
+            $UserItem->query('LOCK TABLES user_item WRITE, user_item as UserItem WRITE');
+
+            // get user inventory
+            $inventory = Hash::combine(
+                $UserItem->findAllByUserId($recipient_id),
+                '{n}.UserItem.item_id', '{n}.UserItem'
+            );
+
+            // add gift details to user items array
+            foreach ($gift['GiftDetail'] as $detail) {
+
+                $item_id = $detail['item_id'];
+                $quantity = $detail['quantity'];
+
+                if (empty($inventory[$item_id])) {
+                    $inventory[$item_id] = array(
+                        'user_id' => $recipient_id,
+                        'item_id' => $item_id,
+                        'quantity' => $quantity
+                    );
+                } else {
+                    $inventory[$item_id]['quantity'] += $quantity;
+                }
+            }
+
+            // save and unlock inventory
+            $UserItem->saveMany($inventory, array('atomic' => false));
+            $UserItem->query('UNLOCK TABLES');
+
+            // set gift as accepted
+            $this->id = $gift_id;
+            $this->saveField('accepted', 1);
+
+            // return gift data so controller can use it
+            return $gift;
+
+        } else {
+
+            // no pending gift found that matched $gift_id and $recipient_id
+            return false;
+        }
+    }
+
+    /**
+     * Returns a query that can be used to fetch a page of gift activity.
+     *
+     * Note: This does not return data; it simply returns the query as an array.
+     *
+     * @param int $limit optional limit for number of gifts to return
+     * @return array
+     */
+    public function getActivityQuery($limit = 5) {
+
+        return array(
+            'Gift' => array(
+                'contain' => 'GiftDetail',
+                'limit' => $limit
+            )
+        );
+    }
 
 
 /**
