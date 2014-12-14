a939fec281cd37c22675cd323adc59f4db69d92c
WhiteThunder <whetethunger@gmail.com>
2014-12-13 23:11:16 -0800

    Added documentation to AccessComponent

--- app/Controller/Component/AccessComponent.php
+++ app/Controller/Component/AccessComponent.php
@@ -1,19 +1,42 @@
 <?php
 App::uses('Component', 'Controller');
 
+/**
+ * Class AccessComponent
+ *
+ * This class is used for making queries about a user's access level, typically for the current user but other users can
+ * also be queried.
+ *
+ * @property AuthComponent $Auth
+ * @property AclComponent $Acl
+ */
 class AccessComponent extends Component {
     public $components = array('Auth', 'Acl');
     public $cache = array();
     public $user;
 
+    /**
+     * Sets up the component.
+     *
+     * @param Controller $controller
+     */
     public function initialize(Controller $controller) {
         $this->user = $this->Auth->user();
     }
 
+    /**
+     * Checks whether the current user can access a specific Access Control Object (ACO) and returns true/false.
+     *
+     * @param string $aco the ACO for which to check access
+     * @param string $action (optional) action on the ACO to check (default '*')
+     * @return bool whether the user can access the specified ACO
+     */
     public function check($aco, $action = '*') {
+
+        // no user means no special access
         if (empty($this->user)) return false;
 
-        //Check for cached perm in this request
+        // return cached perm if available in this request
         if (isset($this->cache[$aco][$action])) {
             return $this->cache[$aco][$action];
         }
@@ -21,7 +44,7 @@ class AccessComponent extends Component {
         $user_id = $this->user['user_id'];
         $canAccess = $this->checkUser($user_id, $aco, $action);
 
-        //Cache perms for later in this request
+        // cache perm for later in this request
         if (empty($this->cache[$aco])) {
             $this->cache[$aco] = array($action => $canAccess);
         } else {
@@ -31,6 +54,13 @@ class AccessComponent extends Component {
         return $canAccess;
     }
 
+    /**
+     * Finds the specified user in the ARO table and returns their id. This is usually used to see if the player is a
+     * member since that is the lowest level of access in the tree. Non-members should not be in the ARO table at all.
+     *
+     * @param int $user_id the user for which to search
+     * @return array the found user or an empty array
+     */
     public function findUser($user_id) {
         return $this->Acl->Aro->find('first', array(
             'fields' => 'id',
@@ -41,14 +71,27 @@ class AccessComponent extends Component {
         ));
     }
 
+    /**
+     * Checks if the specified user is a member and returns true/false.
+     *
+     * @param int $user_id the user to check
+     * @return bool whether the user is a member
+     */
     public function checkIsMember($user_id) {
 
-        //All members and up have records in the aro table
+        // all members and up have records in the aro table
         $aro = $this->findUser($user_id);
         return !empty($aro);
     }
 
-    public function getMembers($ids) {
+    /**
+     * Returns member info for a list of user ids. The result is a list of divisions indexed by user_id, but the
+     * presence of any particular users in the results also speaks to whether the user is a member or not.
+     *
+     * @param array $ids list of users for which to get member info
+     * @return array list of divisions indexed by user_id
+     */
+    public function getMemberInfo($ids) {
 
         return Hash::combine($this->Acl->Aro->find('all', array(
             'fields' => array(
@@ -61,8 +104,16 @@ class AccessComponent extends Component {
         )), '{n}.Aro.foreign_key', '{n}.Aro');
     }
 
+    /**
+     * Checks the access level for a specific user to a specific Access Control Object (ACO) and optional action.
+     *
+     * @param int $user_id the user for which to check access
+     * @param string $aco the ACO for which to check access
+     * @param string $action (optional) action on the ACO to check (default '*')
+     * @return bool whether the user can access the specified ACO
+     */
     public function checkUser($user_id, $aco, $action = '*') {
 
-        return $this->checkIsMember($user_id) ? $this->Acl->check(array('model' => 'User', 'foreign_key' => $user_id), $aco, $action) : false;
+        return $this->Acl->check(array('model' => 'User', 'foreign_key' => $user_id), $aco, $action);
     }
 }
