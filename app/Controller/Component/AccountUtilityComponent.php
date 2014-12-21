e9c194b95e6d1353a1e3b2f9bdb96b7260c0ebce
WhiteThunder <WheteThunger@gmail.com>
2014-12-21 14:06:53 -0800

    Delete SavedLogin cookie if banned

--- app/Controller/Component/AccountUtilityComponent.php
+++ app/Controller/Component/AccountUtilityComponent.php
@@ -54,6 +54,7 @@ class AccountUtilityComponent extends Component {
         if (!($flags & self::LOGIN_SKIP_BAN_CHECK) && $this->isPlayerBanned($steamid)) {
             $this->Session->setFlash('You are currently banned and may not use the store.', 'flash_closable', array('class' => 'error'));
             $this->SavedLogin->deleteForUser($this->AccountIDFromSteamID64($steamid));
+            $this->Cookie->delete('saved_login');
             return false;
         }
 
