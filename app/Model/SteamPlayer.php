be51020186a54ac65e23a66cfb6cb0dca2f0981e
WhiteThunder <WheteThunger@gmail.com>
2014-12-22 19:43:21 -0800

    Added documentation to SteamPlayer model. Log time for API calls

--- app/Model/SteamPlayer.php
+++ app/Model/SteamPlayer.php
@@ -3,12 +3,21 @@
 /**
  * SteamPlayer Model
  *
- *  @property SteamPlayerCache $SteamPlayerCache
+ * @property SteamPlayerCache $SteamPlayerCache
  */
 class SteamPlayer extends AppModel {
     public $useDbConfig = 'steam';
     public $hasOne = 'SteamPlayerCache';
 
+    /**
+     * Gets player data for all of the players in the provided list of steamids. If all of the players are saved in the
+     * cache and have not expired according to Store.SteamCacheDuration, the data will be pulled from the cache. If not,
+     * all players will be re-fetched with the Steam API and saved again. This aggressive approach ensures fewer calls
+     * to the Steam API overall, but the calls that do happen may take longer.
+     *
+     * @param array $steamids list of 64-bit steamids
+     * @return array of player data in no particular order
+     */
     public function getByIds($steamids) {
 
         $cacheDuration = Configure::read('Store.SteamCacheDuration');
@@ -33,10 +42,14 @@ class SteamPlayer extends AppModel {
 
         } else {
 
-            CakeLog::write('steam', "Tried to fetch $countDesired players from the Steam cache but found only $countFound. Steam API used.");
+            $beginTime = microtime(true);
             $steamPlayers = $this->SteamPlayerCache->refresh($steamids);
+            $endTime = microtime(true);
 
-            //Prune expired records
+            $timeTaken = number_format($endTime - $beginTime, 3);
+            CakeLog::write('steam', "Tried to fetch $countDesired players from the Steam cache but found only $countFound. Steam API used. Took $timeTaken seconds.");
+
+            // prune expired records
             $this->SteamPlayerCache->deleteAll(array(
                 'cached <' => $cacheExpireTime
             ));
@@ -45,6 +58,12 @@ class SteamPlayer extends AppModel {
         }
     }
 
+    /**
+     * Gets a single player from the cache by steamid.
+     *
+     * @param int $steamid the 64-bit steamid of the player
+     * @return array of player data for the specified steamid
+     */
     public function getBySteamId($steamid) {
         $players = $this->find('all', array(
             'conditions' => array(
@@ -60,6 +79,12 @@ class SteamPlayer extends AppModel {
         return $player;
     }
 
+    /**
+     * Sets and returns schema info for Steam API.
+     *
+     * @param bool $field
+     * @return array
+     */
     function schema($field = false) {
         $this->_schema = array(
             'steamid' => array('type' => 'String'),
@@ -88,4 +113,3 @@ class SteamPlayer extends AppModel {
     }
 
 }
-?>
