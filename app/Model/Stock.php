779e67cce1eff5514be394f5e462c412ddcb42c9
WhiteThunder <WheteThunger@gmail.com>
2015-06-25 20:49:22 -0700

    #64 Added standalone method for adding stock that returns the actual amount stocked and makes sure stock does not go over the maximum

--- app/Model/Stock.php
+++ app/Model/Stock.php
@@ -19,6 +19,66 @@ class Stock extends AppModel {
 
     public $displayField = 'quantity';
 
+
+    /**
+     * Adds the specified amount of each item to the current stock, up to the current maximum for
+     * each item. This returns an array of quantities actually stocked in case that differs from
+     * what you requested. You should call this before saving a shipment for accurate records.
+     *
+     * @param array $items list of item quantities indexed by item_id
+     * @return array list of item quantities actually stocked
+     */
+    public function addStock($items) {
+
+        $this->query('LOCK TABLES stock WRITE');
+
+        // get current stock for all items in list
+        $allStock = Hash::combine($this->find('all', array(
+            'fields' => array(
+                'item_id', 'quantity', 'maximum'
+            ),
+            'conditions' => array(
+                'item_id' => array_keys($items)
+            )
+        )), '{n}.Stock.item_id', '{n}.Stock');
+
+        // this will store items actually stocked (excluding those that were at maximum or negative)
+        $amountsStocked = array();
+
+        // attempt to modify stocks
+        foreach ($items as $item_id => $addStock) {
+            $stockData = &$allStock[$item_id];
+
+            $currentStock = $stockData['quantity'];
+            $maxStock = $stockData['maximum'];
+
+            // do not stock negative amounts
+            $addStock = max($addStock, 0);
+
+            // do not stock more than up to the maximum
+            $newStock = min($currentStock + $addStock, $maxStock);
+
+            $addStock = $newStock - $currentStock;
+
+            if ($addStock > 0) {
+                $stockData['quantity'] = $newStock;
+                $amountsStocked[$item_id] = $addStock;
+            } else {
+                // no change
+                unset($allStock[$item_id]);
+            }
+        }
+
+        $this->saveMany($allStock, array(
+            'fields' => array('quantity'),
+            'atomic' => false
+        ));
+
+        $this->query('UNLOCK TABLES');
+
+        return $amountsStocked;
+    }
+
 /**
  * Validation rules
  *
